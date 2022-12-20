b701deb94a3440db1813236c546c92c9e0319c57
Gregory Schare <grschare1315@gmail.com>
2022-12-19 22:56:11 -0500

    WIP convert pseudo to cpp

--- translator/Translate.cpp
+++ translator/Translate.cpp
@@ -11,25 +11,78 @@
 using namespace llvm;
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << "bitcode_filename" << std::endl;
-    return 1;
-  }
-  StringRef filename = argv[1];
-  LLVMContext context;
+    // Error and produce help message if user did not provide a filename to translate.
+    if (argc != 2) {
+      std::cerr << "Usage: " << argv[0] << "bitcode_filename" << std::endl;
+      return 1;
+    }
 
-  ErrorOr<std::unique_ptr<MemoryBuffer>> fileOrErr =
-    MemoryBuffer::getFileOrSTDIN(filename);
-  if (std::error_code ec = fileOrErr.getError()) {
-    std::cerr << " Error opening input file: " + ec.message() << std::endl;
-    return 2;
-  }
-  ErrorOr<llvm::Module *> moduleOrErr =
-      parseBitcodeFile(fileOrErr.get()->getMemBufferRef(), context);
-  if (std::error_code ec = fileOrErr.getError()) {
-    std::cerr << "Error reading Moduule: " + ec.message() << std::endl;
-    return 3;
-  }
+    StringRef filename = argv[1];
+    LLVMContext context;
+
+    ErrorOr<std::unique_ptr<MemoryBuffer>> fileOrErr =
+      MemoryBuffer::getFileOrSTDIN(filename);
+    /*
+    if (std::error_code ec = fileOrErr.getError()) {
+      std::cerr << " Error opening input file: " + ec.message() << std::endl;
+      return 2;
+    }
+    */
+    Expected<std::unique_ptr<Module>> moduleOrErr =
+        parseBitcodeFile(fileOrErr.get()->getMemBufferRef(), context);
+    /*
+    if (std::error_code ec = fileOrErr.getError()) {
+      std::cerr << "Error reading Moduule: " + ec.message() << std::endl;
+      return 3;
+    }
+    */
+    std::unique_ptr<Module> m (std::move(moduleOrErr.get()));
+    std::cout << "Successfully read Module:" << std::endl;
+    std::cout << " Name: " << m->getName().str() << std::endl;
+    std::cout << " Target triple: " << m->getTargetTriple() << std::endl;
+
+    for (auto iter1 = m->getFunctionList().begin();
+         iter1 != m->getFunctionList().end(); iter1++) {
+      Function &f = *iter1;
+      std::cout << " Function: " << f.getName().str() << std::endl;
+      for (auto iter2 = f.getBasicBlockList().begin();
+           iter2 != f.getBasicBlockList().end(); iter2++) {
+        BasicBlock &bb = *iter2;
+        std::cout << "  BasicBlock: " << bb.getName().str() << std::endl;
+        for (auto iter3 = bb.begin(); iter3 != bb.end(); iter3++) {
+          Instruction &inst = *iter3;
+          std::cout << "   Instruction " << &inst << " : " << inst.getOpcodeName();
+        }
+      }
+    }
+
+
+    //  bb refers to the basic block number
+    // globalVarState[std:make_pair(bb, "y")] = Add(globalVarState[(bb_prev,"x")], 1)
+    //std::map< std::pair<BasicBlock &, std::string>, SMT_expr> globalVarState;
+    // nextState[1] = { 2, 3, 5 }
+    //std::map<BasicBlock &, std::set<BasicBlock &>> nextState;
+
+    Function *mainFunc = m->getFunction("main");
+    // Loop over basic blocks in a function
+    // Function &Func = ...
+    for (BasicBlock &BB : *mainFunc) {
+      // Print out the name of the basic block if it has one, and then the
+      // number of instructions that it contains
+      errs() << "Basic block (name=" << BB.getName() << ") has "
+                 << BB.size() << " instructions.\n";
+
+        // Loop over instructions in a basic block
+        for (Instruction &I : BB) {
+           // The next statement works since operator<<(ostream&,...)
+           // is overloaded for Instruction&
+           errs() << I << "\n";
+        }
+    }
+}
+
+/*
+int badmain(int argc, char *argv[]) {
 
   Module *m = moduleOrErr.get();
   std::cout << "Successfully read Module:" << std::endl;
@@ -70,3 +123,4 @@ int main(int argc, char *argv[]) {
   }
   return 0;
 }
+*/
